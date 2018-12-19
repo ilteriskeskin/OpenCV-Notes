e7141f61e22a652a1899fe91d0775b316c9d676a
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-12-19 14:56:25 +0300

    Yeni örnekler eklendi

---
+++ kodlar/resim/morphological_transformations_erosion.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('opencv-python.png',0)
+kernel = np.ones((5,5),np.uint8)
+erosion = cv2.erode(img,kernel,iterations = 1)
+
+cv2.imshow('img', img)
+cv2.imshow('erosion', erosion)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
